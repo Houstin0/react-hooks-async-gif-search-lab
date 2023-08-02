7c80f21e45bb4761f65e51ad8b91a52b26464ad3
Houstin Angwenyi <houstin.angwenyi@student.moringaschool.com>
2023-08-02 13:17:54 +0300

    completed assignment

---
+++ src/components/GifList.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+const GifList = ({ gifs }) => {
+  const gifsData = gifs.map((gif) => (
+    <li key={gif.id}>
+      <img src={gif.images.original.url} />
+    </li>
+  ));
+  return (
+    <div>
+      <ul>{gifsData}</ul>
+    </div>
+  );
+};
+
+export default GifList;
